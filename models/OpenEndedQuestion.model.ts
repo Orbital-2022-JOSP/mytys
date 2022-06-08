1e25620a985139b60e25b2db9fba2f2e9ae7bbdf
John Benedict <johnbenedictyan@gmail.com>
2022-06-08 13:30:04 +0800

    Initialised the open ended question model.

---
+++ models/OpenEndedQuestion.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { model, Schema, Types } from 'mongoose';
+
+/**
+ * The open ended question interface that extends from the generic question interface
+ * 
+ * @interface IOpenEndedQuestion
+ * @member {Types.ObjectId} title Title of the question
+ * @member {string} questionType open ended
+ * @member {string} explanation The explanation of the answer to the question whether that be by a tutor or sysAdmin
+ * @member {string} correctAnswer The correct answer of the open ended question
+ */
+
+export interface IOpenEndedQuestion {
+    questionId: Types.ObjectId;
+    questionType: "open ended";
+    correctAnswer: string;
+}
+
+const OpenEndedQuestionSchema = new Schema<IOpenEndedQuestion>({
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Question"
+    },
+    questionType: {
+        type: String,
+        default: "open ended"
+    },
+    correctAnswer: {
+        type: String
+    }
+})
+
+export default mongoose.models.OpenEndedQuestion || model<IOpenEndedQuestion>('OpenEndedQuestion', OpenEndedQuestionSchema)
