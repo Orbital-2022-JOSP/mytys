faa79ad93e7c51a52a4e428ea7d3f76d8805261e
John Benedict <johnbenedictyan@gmail.com>
2022-06-08 13:16:47 +0800

    Initialised the mcq question model.

---
+++ models/MCQQuestion.model.ts
@@ -0,0 +1,41 @@
+import mongoose, { model, Schema, Types } from 'mongoose';
+import { IMCQQuestionOption } from './MCQQuestionOption.model';
+
+/**
+ * The mcq question interface that extends from the generic question interface
+ * 
+ * @interface IMCQQuestion
+ * @member {Types.ObjectId} title Title of the question
+ * @member {string} questionType mcq
+ * @member {IMCQQuestionOption} correctAnswer The correct answer of the mcq question
+ * @member {Array<IMCQQuestionOption>} options The options of the mcq question
+ */
+
+export interface IMCQQuestion {
+    questionId: Types.ObjectId;
+    questionType: "mcq";
+    correctAnswer: IMCQQuestionOption;
+    options: Array<IMCQQuestionOption>;
+    
+}
+
+const MCQQuestionSchema = new Schema<IMCQQuestion>({
+    questionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Question"
+    },
+    questionType: {
+        type: String,
+        default: "mcq"
+    },
+    correctAnswer: {
+        type: Schema.Types.ObjectId,
+        ref: "MCQQuestionOption"
+    },
+    options: [{
+        type: Schema.Types.ObjectId,
+        ref: "MCQQuestionOption"
+    }]
+})
+
+export default mongoose.models.MCQQuestion || model<IMCQQuestion>('MCQQuestion', MCQQuestionSchema)
