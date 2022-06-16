d32e87ab57f00342e3f903a3496ce95effd5dbbf
John Benedict <johnbenedictyan@gmail.com>
2022-06-16 12:34:03 +0800

    Added the mcq question routes.

---
+++ pages/api/question/mcq/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from '../../../../dbConnect';
+import MCQQuestionModel from "../../../../models/MCQQuestion.model";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+
+    await dbConnect();
+
+    switch (method) {
+        case 'GET':
+            try {
+                const Questions = await MCQQuestionModel.find({});
+                res.status(201).json({ success: true, data: Questions });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+
+        default:
+            res.status(404).end();
+            break;
+    }
+}
