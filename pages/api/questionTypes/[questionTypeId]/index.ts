c14d0467566bb95b2666245e658d9cac9baa8e47
John Benedict <johnbenedictyan@gmail.com>
2022-06-16 17:16:59 +0800

    Added the question types routes.

---
+++ pages/api/questionTypes/[questionTypeId]/index.ts
@@ -0,0 +1,57 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from '../../../../dbConnect';
+import QuestionTypeModel from "../../../../models/QuestionType.model";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {
+        query: { questionTypeId },
+        method,
+    } = req
+
+    await dbConnect();
+
+    switch (method) {
+        case 'GET' /* Get a model by its ID */:
+            try {
+                const questionType = await QuestionTypeModel.findById(questionTypeId);
+                if (!QuestionTypeModel) {
+                    return res.status(400).json({ success: false });
+                }
+                res.status(200).json({ success: true, data: questionType });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+
+        case 'PUT' /* Edit a model by its ID */:
+            try {
+                const questionType = await QuestionTypeModel.findByIdAndUpdate(questionTypeId, req.body, {
+                    new: true,
+                    runValidators: true,
+                });
+                if (!questionType) {
+                    return res.status(400).json({ success: false });
+                }
+                res.status(200).json({ success: true, data: questionType });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+
+        case 'DELETE' /* Delete a model by its ID */:
+            try {
+                const deletedQuestionType = await QuestionTypeModel.deleteOne({ _id: questionTypeId });
+                if (!deletedQuestionType) {
+                    return res.status(400).json({ success: false });
+                }
+                res.status(200).json({ success: true, data: {} });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
+}
