210a050c04ebed6a3e1ca583e3a435451cf1713e
John Benedict <johnbenedictyan@gmail.com>
2022-06-20 17:03:10 +0800

    Modified the navbar to inclue the real logo.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -14,23 +14,24 @@ export const Navbar = () => {
                     title="Company"
                     className="inline-flex items-center"
                 >
-                    <svg
+                    <img
+                        src='/logo.svg'
                         className="w-8 text-deep-purple-accent-400"
-                        viewBox="0 0 24 24"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeMiterlimit="10"
-                        stroke="currentColor"
-                        fill="none"
-                    >
-                        <rect x="3" y="1" width="7" height="12" />
+                        // viewBox="0 0 24 24"
+                        // strokeLinejoin="round"
+                        // strokeWidth="2"
+                        // strokeLinecap="round"
+                        // strokeMiterlimit="10"
+                        // stroke="currentColor"
+                        // fill="none"
+                    />
+                        {/* <rect x="3" y="1" width="7" height="12" />
                         <rect x="3" y="17" width="7" height="6" />
                         <rect x="14" y="1" width="7" height="6" />
-                        <rect x="14" y="11" width="7" height="12" />
-                    </svg>
+                        <rect x="14" y="11" width="7" height="12" /> */}
+                    {/* </svg> */}
                     <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                        Company
+                        MyTYS
                     </span>
                 </a>
                 <ul className="flex items-center hidden space-x-8 lg:flex">
