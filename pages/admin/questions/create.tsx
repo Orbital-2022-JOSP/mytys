1036dcb660893c39b7afc0059077ef6259365e05
John Benedict <johnbenedictyan@gmail.com>
2022-06-24 13:29:27 +0800

    Updated the admin component.

---
+++ pages/admin/questions/create.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminQuestionsCreatePage: React.FC = () => {
+    return (
+        <>
+            <p>Create new question page</p>
+        </>
+    );
+};
+
+export default AdminQuestionsCreatePage;
