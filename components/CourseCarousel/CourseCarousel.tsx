080b3279dec1b036e6912b7646f046e27419ecfd
John Benedict <johnbenedictyan@gmail.com>
2022-06-29 19:47:55 +0800

    Removed the unnecessary link.

--- components/CourseCarousel/CourseCarousel.tsx
+++ components/CourseCarousel/CourseCarousel.tsx
@@ -125,7 +125,7 @@ export const CourseCarousel: React.FC = () => {
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
                 <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-                    <a href="/" className="mb-6 sm:mx-auto">
+                    <div className="mb-6 sm:mx-auto">
                         <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
                             <svg
                                 className="w-10 h-10 text-deep-purple-accent-400"
@@ -141,7 +141,7 @@ export const CourseCarousel: React.FC = () => {
                                 />
                             </svg>
                         </div>
-                    </a>
+                    </div>
                     <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
                         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
                             <span className="relative inline-block">
