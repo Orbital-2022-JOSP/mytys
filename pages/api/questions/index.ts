79de873987cc7668067bb2f839d853e13a49ee19
John Benedict <johnbenedictyan@gmail.com>
2022-06-30 22:22:46 +0800

    Updated the question create api route.

--- pages/api/questions/index.ts
+++ pages/api/questions/index.ts
@@ -3,6 +3,7 @@ import dbConnect from "../../../dbConnect";
 import QuestionModel from "../../../models/Question.model";
 import QuestionTopicModel from "../../../models/QuestionTopic.model";
 import QuestionTypeModel from "../../../models/QuestionType.model";
+import sanitize from 'mongo-sanitize';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { method } = req;
@@ -24,6 +25,44 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     })
                 res.status(201).json({ success: true, data: questions });
             } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+
+        case 'POST':
+            const { questionBody, mcqQuestionBody } = sanitize(req.body);
+            try {
+                const question = await QuestionModel.create(questionBody);
+
+                const mcqQuestionBodyTypeSafe: mcqQuestionBodySchema = mcqQuestionBody
+                const options = mcqQuestionBodyTypeSafe.options;
+                const correctOption = mcqQuestionBodyTypeSafe.correctAnswer;
+
+                const asd = await MCQQuestionOptionModel.create({
+                    questionId: question._id,
+                    text: correctOption.text
+                })
+
+                const filteredOptions = options.filter((x) => x.text != correctOption.text).map((x) => ({
+                    ...x,
+                    questionId: question._id
+                }));
+
+                const qwe = await MCQQuestionOptionModel.insertMany(filteredOptions)
+
+                const allOptionsId = qwe.map(x => x._id)
+                allOptionsId.push(asd._id);
+
+                // Something might go wrong if the question model creates successfully but the mcq question doesn't 
+                // Then you have a floating question object in db.
+                const mcqQuestion = await MCQQuestionModel.create({
+                    questionId: question._id,
+                    correctAnswer: asd._id,
+                    options: allOptionsId
+                })
+                res.status(201).json({ success: true, data: mcqQuestion });
+            } catch (error) {
+                console.log(error)
                 res.status(400).json({ success: false });
             }
             break;
