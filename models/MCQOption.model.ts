08c0b74ebd45c605bf6c86857336f2c87c2ca1ed
John Benedict <johnbenedictyan@gmail.com>
2022-06-30 22:05:11 +0800

    Moved the models backed to one sinlge table for all question types.

--- models/MCQOption.model.ts
+++ models/MCQOption.model.ts
@@ -3,22 +3,22 @@ import mongoose, { model, Schema, Types } from 'mongoose';
 /**
  * The mcq question option interface
  * 
- * @interface IMCQQuestionOption
+ * @interface IMCQOption
  * @member {Types.ObjectId} id The id of the mcq question option
  * @member {Types.ObjectId} questionId The corresponding question id
  * @member {string} text The text of the question option
  */
 
-export interface IMCQQuestionOption {
+export interface IMCQOption {
     _id?: Types.ObjectId;
     questionId: Types.ObjectId;
     text: string;
 }
 
-const MCQQuestionOptionSchema = new Schema<IMCQQuestionOption>({
+export const MCQOptionSchema = new Schema<IMCQOption>({
     questionId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'MCQQuestion'
+        ref: 'MCQ'
     },
     text: {
         type: String
@@ -27,4 +27,4 @@ const MCQQuestionOptionSchema = new Schema<IMCQQuestionOption>({
     timestamps: true
 })
 
-export default mongoose.models.MCQQuestionOption || model<IMCQQuestionOption>('MCQQuestionOption', MCQQuestionOptionSchema)
+export default mongoose.models.MCQOption || model<IMCQOption>('MCQOption', MCQOptionSchema)
