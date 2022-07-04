a2aaca8951439c0e5a85458aeb2c2884a94be8d2
John Benedict <johnbenedictyan@gmail.com>
2022-07-04 21:35:25 +0800

    Updated the course single question page.

--- pages/courses/[id]/question/[questionIdx].tsx
+++ pages/courses/[id]/question/[questionIdx].tsx
@@ -1,16 +1,13 @@
-import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import useSWR from 'swr';
-import { MCQQuestionAttempt } from '../../../../components/MCQQuestionAttempt/MCQQuestionAttempt';
+import { Question } from '../../../../components/Question/Question';
+import { useState, useEffect } from 'react';
 
 const CourseQuestionPage: React.FC = () => {
     const router = useRouter()
     const { id, questionIdx } = router.query
-
-    console.log(id, questionIdx);
-
     const { data: session, status } = useSession();
-
     const fetcher = (url: string) => fetch(url).then(r => r.json());
     const { data: courseData, error } = useSWR(
         id
@@ -18,28 +15,37 @@ const CourseQuestionPage: React.FC = () => {
             : null
         , fetcher
     );
+    const [validQn, setValidQn] = useState(false);
 
-    if(courseData){
-        let questionId = courseData.data.questions[questionIdx as string];
-        if(questionId){
-            console.log(questionId)
-        } else {
-            // Redirect to course page
-        }
-    }
+    useEffect(() => {
+        setValidQn(parseInt(questionIdx as string) >= 0 && parseInt(questionIdx as string) <= courseData.data.questions.length)
+    }, []);
 
     return (
-        // <MCQQuestionAttempt
-        //     questionId={data.data._id}
-        //     title={data.data.title}
-        //     description={data.data.description}
-        //     explanation={data.data.explanation}
-        //     correctAnswer={data.data.mcqQuestions[0].correctAnswer}
-        //     options={data.data.mcqQuestions[0].options}
-        //     nextLink={"/questions/mcq/random"}
-        //     prevLink={"/questions/mcq/random"}
-        // />
-        <p>asd</p>
+        <>
+            {
+                courseData && validQn
+                    ? <Question {...courseData.data.questions[questionIdx as string]} prevLink={""} nextLink={""} />
+                    : <p>Loaded</p>
+                // status == "loading"
+                //     ? <p>Loading</p>
+                //     : status == "unauthenticated"
+                //         ? <Unauthenticated />
+                //         : data && data.success
+                //             ? <p>Loaded</p>
+                //             // <MCQQuestionAttempt
+                //             //     questionId={data.data._id}
+                //             //     title={data.data.title}
+                //             //     description={data.data.description}
+                //             //     explanation={data.data.explanation}
+                //             //     correctAnswer={data.data.mcqQuestions[0].correctAnswer}
+                //             //     options={data.data.mcqQuestions[0].options}
+                //             //     nextLink={"/questions/mcq/random"}
+                //             //     prevLink={"/questions/mcq/random"}
+                //             // />
+                //             : <p>Loading</p>
+            }
+        </>
     )
 }
 
