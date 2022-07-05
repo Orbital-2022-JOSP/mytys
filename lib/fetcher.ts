93f98bf2971dc4a74d502a5f7998d3883ea51d25
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 16:51:06 +0800

    Updated the fetcher function such that it will return an error status code if there was an api error.

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,1 +1,20 @@
-export const fetcher = (url: string) => fetch(url).then(r => r.json());
+interface APIError extends globalThis.Error {
+    info?: any;
+    status?: number;
+}
+
+export const fetcher = async url => {
+    const res = await fetch(url)
+
+    // If the status code is not in the range 200-299,
+    // we still try to parse and throw it.
+    if (!res.ok) {
+        const error: APIError = new Error('An error occurred while fetching the data.')
+        // Attach extra info to the error object.
+        error.info = await res.json()
+        error.status = res.status
+        throw error
+    }
+
+    return res.json()
+}
