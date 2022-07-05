1a2b59a873aab157cf5cff575d38f6344c29be42
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 16:54:06 +0800

    Edited the single course page so that it will display not found if the course cannot be found.

--- pages/courses/[id]/index.tsx
+++ pages/courses/[id]/index.tsx
@@ -1,21 +1,23 @@
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import LoadingComponent from '../../../components/LoadingComponent/LoadingComponent';
+import { NotFound } from '../../../components/NotFound/NotFound';
 import { SingleCourse } from '../../../components/SingleCourse/SingleCourse';
 import { fetcher } from '../../../lib/fetcher';
 
 const SingleCoursePage: React.FC = () => {
     const router = useRouter()
     const { id } = router.query
-
-    const { data: courseData, error } = useSWR(`/api/courses/${id}`, fetcher);
+    const { data: courseData, error: courseError } = useSWR(`/api/courses/${id}`, fetcher);
 
     return (
         <>
             {
-                courseData
-                    ? <SingleCourse {...courseData.data} />
-                    : <LoadingComponent />
+                courseError.status == 404
+                    ? <NotFound />
+                    : courseData
+                        ? <SingleCourse {...courseData.data} />
+                        : <LoadingComponent />
             }
         </>
     )
