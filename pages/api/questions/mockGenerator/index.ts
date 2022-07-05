bb4ebc53e1cab87733f72d2ea9958ae3dd751c49
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 11:56:31 +0800

    Updated the mock generator.

--- pages/api/questions/mockGenerator/index.ts
+++ pages/api/questions/mockGenerator/index.ts
@@ -14,37 +14,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         case 'GET':
             try {
                 fs.readFile('./lib/mytys_mock_question.json', (err, data) => {
-                    if (err) throw err;
-                    let questions = JSON.parse(data);
-                    questions.forEach(async questionData => {
-                        if (questionData.questionType == "mcq") {
-                            const mcqCorrectAnswer = questionData.mcqCorrectAnswer;
-                            const mcqOptions = questionData.mcqOptions;
-                            delete questionData.mcqCorrectAnswer;
-                            delete questionData.mcqOptions;
+                    if (err) {
+                        throw err;
+                    } else {
+                        let questions = JSON.parse(data);
+                        questions.forEach(async questionData => {
+                            if (questionData.questionType == "mcq") {
+                                const mcqCorrectAnswer = questionData.mcqCorrectAnswer;
+                                const mcqOptions = questionData.mcqOptions;
+                                delete questionData.mcqCorrectAnswer;
+                                delete questionData.mcqOptions;
 
-                            const newQuestion = new QuestionModel(questionData)
-                            const newCorrectMCQOption = await MCQOptionModel.create({
-                                questionId: newQuestion._id,
-                                text: mcqCorrectAnswer.text
-                            })
+                                const newQuestion = new QuestionModel(questionData)
+                                const newCorrectMCQOption = await MCQOptionModel.create({
+                                    questionId: newQuestion._id,
+                                    text: mcqCorrectAnswer.text
+                                })
 
-                            const filteredOptions = mcqOptions.filter((x) => x.text != newCorrectMCQOption.text).map((x) => ({
-                                ...x,
-                                questionId: newQuestion._id
-                            }));
+                                const filteredOptions = mcqOptions.filter((x) => x.text != newCorrectMCQOption.text).map((x) => ({
+                                    ...x,
+                                    questionId: newQuestion._id
+                                }));
 
-                            const newOtherOptions = await MCQOptionModel.insertMany(filteredOptions)
-                            const optionsIds = newOtherOptions.map((x) => x._id)
-                            optionsIds.push(newCorrectMCQOption._id);
+                                const newOtherOptions = await MCQOptionModel.insertMany(filteredOptions)
+                                const optionsIds = newOtherOptions.map((x) => x._id)
+                                optionsIds.push(newCorrectMCQOption._id);
 
-                            newQuestion.mcqCorrectAnswer = newCorrectMCQOption._id;
-                            newQuestion.mcqOptions = optionsIds;
-                            newQuestion.save();
-                        } else {
-                            QuestionModel.create(questionData);
-                        }
-                    });
+                                newQuestion.mcqCorrectAnswer = newCorrectMCQOption._id;
+                                newQuestion.mcqOptions = optionsIds;
+                                newQuestion.save();
+                            } else {
+                                QuestionModel.create(questionData);
+                            }
+                        });
+                    }
                 });
             } catch (error) {
                 res.status(400).end();
