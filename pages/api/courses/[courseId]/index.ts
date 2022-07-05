e567c458fba3be969eb4a3d7e7c80749b70355d2
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 16:52:22 +0800

    Updated the status code for the course api if no course is found.

--- pages/api/courses/[courseId]/index.ts
+++ pages/api/courses/[courseId]/index.ts
@@ -52,7 +52,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     })
                     .setOptions({ sanitizeFilter: true });
                 if (!course) {
-                    return res.status(400).json({ success: false });
+                    return res.status(404).json({ success: false });
                 }
                 res.status(200).json({ success: true, data: course });
             } catch (error) {
