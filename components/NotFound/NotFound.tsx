bb71ebce4640fcce3a739946b980cab271c32c47
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 12:24:40 +0800

    Initialised the not found component.'

---
+++ components/NotFound/NotFound.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+export const NotFound: React.FC = () => {
+    return (
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+            <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+                <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
+                    <div className="flex items-center justify-center w-120 rounded-full">
+                        <img src="/images/not-found.svg" alt="Picture of Books" className="w-full" />
+                    </div>
+                    <div className="max-w-xl mt-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                            <span className="relative inline-block">
+                                <svg
+                                    viewBox="0 0 52 24"
+                                    fill="currentColor"
+                                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                                >
+                                    <defs>
+                                        <pattern
+                                            id="e77df901-b9d7-4b9b-822e-16b2d410795b"
+                                            x="0"
+                                            y="0"
+                                            width=".135"
+                                            height=".30"
+                                        >
+                                            <circle cx="1" cy="1" r=".7" />
+                                        </pattern>
+                                    </defs>
+                                    <rect
+                                        fill="url(#e77df901-b9d7-4b9b-822e-16b2d410795b)"
+                                        width="52"
+                                        height="24"
+                                    />
+                                </svg>
+                                <span className="relative">Content</span>
+                            </span>{' '}
+                            Not Found
+                        </h2>
+                        <p className="text-base text-gray-700 md:text-lg">
+                            Sorry we can't find that page
+                        </p>
+                    </div>
+                    <div>
+                        <Link href={"/"}>
+                            <a
+                                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-light-blue-400 hover:bg-light-blue-700 focus:shadow-outline focus:outline-none"
+                            >
+                                Return Home
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
