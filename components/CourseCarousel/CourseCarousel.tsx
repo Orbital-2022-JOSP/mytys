c78ef811a37aedad65b4be6f297384dfed97e9f0
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 15:05:55 +0800

    Updated the course carousel to utilise the backend integration.

--- components/CourseCarousel/CourseCarousel.tsx
+++ components/CourseCarousel/CourseCarousel.tsx
@@ -1,19 +1,13 @@
+import useSWR from 'swr';
+import { fetcher } from "../../lib/fetcher";
 import { CourseCard } from "../CourseCard/CourseCard";
+import LoadingComponent from '../LoadingComponent/LoadingComponent';
 
 export const CourseCarousel: React.FC = () => {
-    const courses = [
-        { "courseType": "exuding", "date": "06.02.2022", "title": "De-engineered", "description": "magna ac consequat metus sapien ut nunc vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit", "tags": ["Realigned", "migration", "Face to face", "context-sensitive", "model"], "authorName": "Lynnet Oosthout de Vree", "noOfViews": 7424, "noOfLikes": 3980 },
-        { "courseType": "Exclusive", "date": "22.11.2021", "title": "projection", "description": "nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis aliquam convallis", "tags": ["Monitored", "Quality-focused", "Exclusive", "migration", "hub"], "authorName": "Nadiya Gosforth", "noOfViews": 3059, "noOfLikes": 9542 },
-        { "courseType": "circuit", "date": "14.11.2021", "title": "maximized", "description": "at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel", "tags": ["Object-based", "attitude", "impactful", "Customer-focused", "reciprocal"], "authorName": "Pamella Jackways", "noOfViews": 7035, "noOfLikes": 3640 },
-        { "courseType": "Monitored", "date": "09.08.2021", "title": "discrete", "description": "erat vestibulum sed magna at nunc commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est risus auctor sed tristique", "tags": ["methodical", "installation", "Polarised", "Public-key", "service-desk"], "authorName": "Flemming Allmen", "noOfViews": 9504, "noOfLikes": 1625 },
-        { "courseType": "24/7", "date": "10.11.2021", "title": "client-driven", "description": "risus dapibus augue vel accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer a nibh in quis justo maecenas rhoncus aliquam lacus morbi quis tortor", "tags": ["Versatile", "Virtual", "support", "Stand-alone", "hub"], "authorName": "Mozes Mariot", "noOfViews": 5534, "noOfLikes": 3550 },
-        { "courseType": "Up-sized", "date": "21.03.2022", "title": "task-force", "description": "amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat", "tags": ["Virtual", "portal", "Diverse", "Balanced", "logistical"], "authorName": "Pavel Kurth", "noOfViews": 9300, "noOfLikes": 2791 },
-        { "courseType": "Multi-layered", "date": "04.08.2021", "title": "synergy", "description": "sagittis nam congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur", "tags": ["Operative", "Programmable", "open system", "toolset", "software"], "authorName": "Benoite Lorait", "noOfViews": 8585, "noOfLikes": 2277 },
-        { "courseType": "reciprocal", "date": "12.02.2022", "title": "solution-oriented", "description": "lectus aliquam sit amet diam in magna bibendum imperdiet nullam orci pede venenatis non sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus", "tags": ["bandwidth-monitored", "Cross-group", "Focused", "optimal", "Business-focused"], "authorName": "Rochester Sigmund", "noOfViews": 3961, "noOfLikes": 9609 },
-        { "courseType": "Compatible", "date": "01.01.2022", "title": "Quality-focused", "description": "maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis aliquam", "tags": ["De-engineered", "neural-net", "Fully-configurable", "Reactive", "Advanced"], "authorName": "Kirk Petrazzi", "noOfViews": 3672, "noOfLikes": 8498 },
-    ]
+    const { data: courses, error } = useSWR('/api/courses', fetcher);
+
     return (
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl sm:min-w-xl md:min-w-full lg:min-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
                 <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
                     <div className="mb-6 sm:mx-auto">
@@ -71,9 +65,11 @@ export const CourseCarousel: React.FC = () => {
             </div>
             <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
                 {
-                    courses.map((course, idx) => (
-                        <CourseCard {...course} key={idx} />
-                    ))
+                    courses && courses.data
+                        ? courses.data.map((course, idx) => (
+                            <CourseCard {...course} key={idx} />
+                        ))
+                        : <LoadingComponent />
                 }
             </div>
         </div>
