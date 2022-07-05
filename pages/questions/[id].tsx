71d92b046bdd33b99394e875d4c9bc9b59db0e4d
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 20:31:42 +0800

    Updated the question answering page.

--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -1,43 +1,23 @@
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import LoadingComponent from '../../components/LoadingComponent/LoadingComponent';
+import { NotFound } from '../../components/NotFound/NotFound';
 import { Question } from '../../components/Question/Question';
-// import { MCQQuestionAttempt } from '../../../components/MCQQuestionAttempt/MCQQuestionAttempt';
-// import { Unauthenticated } from '../../../components/Unauthenticated/Unauthenticated';
 import { fetcher } from '../../lib/fetcher';
 
 const MCQQuestionAnsweringPage: React.FC = () => {
     const router = useRouter();
-    const { data: session, status } = useSession();
-
     const { id } = router.query;
-
-    const { data, error } = useSWR(id ? `/api/questions/${id}` : null, fetcher);
-    console.log(data)
+    const { data: questionData, error: questionError } = useSWR(id ? `/api/questions/${id}` : null, fetcher);
 
     return (
         <>
             {
-                data
-                    ? <Question {...data.data} prevLink={""} nextLink={""} />
-                    : <p>Loaded</p>
-                // status == "loading"
-                //     ? <p>Loading</p>
-                //     : status == "unauthenticated"
-                //         ? <Unauthenticated />
-                //         : data && data.success
-                //             ? <p>Loaded</p>
-                //             // <MCQQuestionAttempt
-                //             //     questionId={data.data._id}
-                //             //     title={data.data.title}
-                //             //     description={data.data.description}
-                //             //     explanation={data.data.explanation}
-                //             //     correctAnswer={data.data.mcqQuestions[0].correctAnswer}
-                //             //     options={data.data.mcqQuestions[0].options}
-                //             //     nextLink={"/questions/mcq/random"}
-                //             //     prevLink={"/questions/mcq/random"}
-                //             // />
-                //             : <p>Loading</p>
+                questionError && questionError.status == 404
+                    ? <NotFound />
+                    : questionData
+                        ? <Question {...questionData.data} prevLink={""} nextLink={""} />
+                        : <LoadingComponent />
             }
         </>
     )
