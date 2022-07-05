d20dcdefc2fac960e6c5f4fd4dd1f2acce710d53
John Benedict <johnbenedictyan@gmail.com>
2022-07-05 15:39:34 +0800

    Updated the single course page so to utilise the backend integration.

--- pages/courses/[id]/index.tsx
+++ pages/courses/[id]/index.tsx
@@ -1,13 +1,22 @@
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import LoadingComponent from '../../../components/LoadingComponent/LoadingComponent';
 import { SingleCourse } from '../../../components/SingleCourse/SingleCourse';
+import { fetcher } from '../../../lib/fetcher';
 
 const SingleCoursePage: React.FC = () => {
     const router = useRouter()
     const { id } = router.query
 
+    const { data: courseData, error } = useSWR(`/api/courses/${id}`, fetcher);
+
     return (
         <>
-            <SingleCourse />
+            {
+                courseData
+                    ? <SingleCourse {...courseData.data} />
+                    : <LoadingComponent />
+            }
         </>
     )
 }
