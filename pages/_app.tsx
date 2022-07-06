ab24285b6f3b6035917d18bf9a0a4fb73b8001e7
John Benedict <johnbenedictyan@gmail.com>
2022-07-06 17:39:03 +0800

    Updates.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
-import Head from "next/head";
 import { AdminLayout } from '../components/AdminLayout/AdminLayout';
 import { FooterWithContent } from "../components/FooterWithContent/FooterWithContent";
 import { Navbar } from '../components/Navbar/Navbar';
