583b07a4b42e79f198c3a494ed9719b4a9ad3d5f
John Benedict <johnbenedictyan@gmail.com>
2022-07-06 17:35:40 +0800

    Updates.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,6 @@ import '../styles/globals.css';
 const App = ({ Component, pageProps, router }: AppProps) => {
     return (
         <SessionProvider session={pageProps.session}>
-            <Head>
-                <title>MyTYS</title>
-                {/* <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-                    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" /> */}
-                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-            </Head>
             {
                 router.pathname.startsWith("/admin")
                     ?
