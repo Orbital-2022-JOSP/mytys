0315bd651d1aa1a14ff74148632caef413db0945
John Benedict <johnbenedictyan@gmail.com>
2022-07-20 18:15:01 +0800

    Replaced who are we 1 with who are we component.

--- pages/aboutus.tsx
+++ pages/aboutus.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { JoinUsCTA1 } from "../components/JoinUsCTA1/JoinUsCTA1";
 import { Stats } from "../components/Stats/Stats";
 import { Team1 } from "../components/Team1/Team1";
-import { WhoAreWe1 } from "../components/WhoAreWe1/WhoAreWe1";
+import { WhoAreWe } from '../components/WhoAreWe/WhoAreWe';
 
 const AboutUsPage1: React.FC = () => {
     return (
@@ -11,7 +11,7 @@ const AboutUsPage1: React.FC = () => {
             <Head>
                 <title>About Us - MYTYS</title>
             </Head>
-            <WhoAreWe1 />
+            <WhoAreWe />
             <Team1 />
             <Stats />
             <JoinUsCTA1 />
