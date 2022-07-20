92b548d5198ca471a22ad416a16ebdb87e9ff4b6
John Benedict <johnbenedictyan@gmail.com>
2022-07-20 18:14:54 +0800

    Replaced who are we 1 with who are we component.

--- components/WhoAreWe/WhoAreWe.tsx
+++ components/WhoAreWe/WhoAreWe.tsx
@@ -8,20 +8,16 @@ export const WhoAreWe: React.FC = () => {
                 <div className="flex flex-col lg:flex-row">
                     <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
                         <h2 className="font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                            The quick, brown fox
-                            <br className="hidden md:block" />
-                            jumps over{' '}
+                            What exactly is{" "}
                             <span className="inline-block text-light-blue-accent-400">
-                                a lazy dog
+                                MYTYS
                             </span>
                         </h2>
                     </div>
                     <div className="lg:w-1/2">
                         <p className="mb-4 text-base text-gray-700">
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                            accusantium doloremque rem aperiam, eaque ipsa quae. Sed ut
-                            perspiciatis unde omnis iste. Sed ut perspiciatis unde omnis iste
-                            error sit voluptatem accusantium doloremque rem aperiam.
+                            MYTYS is an all-in-one educational webpage that offers a gamified, tuition-like experience as an alternative to conventional, costly tuition.
+                            MyTYS is created by <span className="text-light-blue-accent-400">students</span>, for <span className="text-light-blue-accent-400">students</span>
                         </p>
                         <Link href={"/"}>
                             <a
