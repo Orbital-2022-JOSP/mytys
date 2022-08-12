ac4a3051be6ce7df46a8e15421b1c4162b54f9dc
John Benedict Yan <johnbenedictyan@gmail.com>
2022-08-12 15:46:44 +0800

    Added loading animations.

--- pages/questions/random/index.tsx
+++ pages/questions/random/index.tsx
@@ -8,22 +8,27 @@ import { fetcher } from '../../../lib/fetcher';
 
 const RandomMCQQuestionAnsweringPage: React.FC = () => {
     const [questionId, setQuestionId] = useState("");
+    const [loading, setLoading] = useState(true);
     const { data: questionIdData, error: questionIdDataError } = useSWR(`/api/questions/random`, fetcher);
     const { data: questionData, error: questionError } = useSWR(questionId ? `/api/questions/${questionId}` : null, fetcher);
-    
+
     const nextQuestionFunc = () => {
+        setLoading(true);
         fetch(
             '/api/questions/random',
         )
             .then(res => res.json())
             .then(res => {
                 setQuestionId(res.data._id);
+                setTimeout(() => { }, 1000);
+                setLoading(false);
             })
     }
 
     useEffect(() => {
         if (questionIdData) {
             setQuestionId(questionIdData.data._id);
+            setLoading(false);
         }
     }, [questionIdData]);
 
@@ -35,7 +40,7 @@ const RandomMCQQuestionAnsweringPage: React.FC = () => {
             {
                 questionError && questionError.status == 404
                     ? <NotFound />
-                    : questionData
+                    : !loading && questionData
                         ? <Question {...questionData.data} prevLink={""} nextLink={""} nextQuestionFunc={nextQuestionFunc} />
                         : <LoadingComponent />
             }
