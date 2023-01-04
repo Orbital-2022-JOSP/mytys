7fabbc3cb6c824337724b3bbfaefc94cbe16c2a6
John Benedict Yan <johnbenedictyan@gmail.com>
2023-01-04 20:28:11 +0800

    Update admin layout

--- components/AdminLayout/AdminLayout.tsx
+++ components/AdminLayout/AdminLayout.tsx
@@ -60,7 +60,7 @@ export const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
                     </button>
                 </div> */}
             </aside>
-            <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] min-h-screen">
+            <div className="ml-auto lg:w-[75%] xl:w-[80%] 2xl:w-[85%] min-h-screen">
                 <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
                     <div className="px-6 flex items-center justify-between space-x-4 2xl:container">
                         <h5 hidden className="text-2xl text-gray-600 font-medium lg:block">Dashboard</h5>
@@ -79,7 +79,7 @@ export const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
                     </div>
                 </div>
 
-                <div className="p-6 2xl:container bg-gray-100 min-h-screen">
+                <div className="pt-10 bg-gray-100 min-h-screen">
                     {children}
                 </div>
             </div>
